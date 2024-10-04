dab0ae3f1dc9b3cf42efed9e34b0c8d5872e34e0
aparnasri4512 <as3929@scarletmail.rutgers.edu>
2024-10-04 17:19:29 -0400

    apps1

--- src/projecttwo/main.java
+++ src/projecttwo/main.java
@@ -3,7 +3,9 @@ package projecttwo;
 public class main {
     public static void main(String[] args){
         System.out.println("hello world!");
+        System.out.println("Aparna!");
     }
+
 
 
 }
