9dea6d0eae5086224aba7e3c209fb60bf3f9f9d2
Gursimar-K-Singh <gks44@scarletmail.rutgers.edu>
2024-10-06 01:17:35 -0400

    Add new classes

---
+++ src/util/ClinicManager.java
@@ -0,0 +1,16 @@
+package util;
+
+public class ClinicManager {
+
+    public ClinicManager() {
+    }
+
+    public void run() {
+        System.out.println("Clinic Manager is running.");
+
+    }
+
+
+
+
+}
