5ff5e8aa95097b0e2d2aee1a8d81b0b9e231f770
Gursimar-K-Singh <gks44@scarletmail.rutgers.edu>
2024-10-11 19:41:13 -0400

    change in sort

---
+++ src/util/Sort.java
@@ -0,0 +1,10 @@
+package util;
+
+/**
+ * A utility class that provides static methods for sorting lists by different keys.
+ * This class should be used to sort appointments, providers, or other objects.
+ */
+
+public class Sort {
+
+}
