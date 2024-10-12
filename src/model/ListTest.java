0115441885b6e88ba9a813e475d1d046f30224d2
Gursimar-K-Singh <gks44@scarletmail.rutgers.edu>
2024-10-12 18:59:02 -0400

    dateTest

---
+++ src/model/ListTest.java
@@ -0,0 +1,16 @@
+package model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ListTest {
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void remove() {
+    }
+}
